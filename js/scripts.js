fe66d128cdb714c64ff452966dbe88fac6d23ee3
rdtyaaa <yogaradit123@gmail.com>
2024-04-01 09:51:17 +0700

    Add Standings

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 const APIkey =
-  "9f939a8e9ff488d14f0230d016c2cf297b9fc459e9b53ffd7c3319d5f4312f1e";
+  "ab9d4fd895dfbd6aa1c8238b7109fa13bdf85ddbd7b27e95d29f3070039450f8";
 
 const countryDropdown = document.getElementById("country-dropdown");
+const leagueDropdown = document.getElementById("league-dropdown");
 
-fetch(`https://apiv3.apifootball.com/?action=get_leagues&APIkey=` + APIkey, {
+fetch(`https://apiv3.apifootball.com/?action=get_countries&APIkey=${APIkey}`, {
   method: "GET",
 })
   .then((response) => response.json())
@@ -19,6 +20,67 @@ fetch(`https://apiv3.apifootball.com/?action=get_leagues&APIkey=` + APIkey, {
   .catch((error) => {
     console.error("Error:", error);
   });
+
+fetch(`https://apiv3.apifootball.com/?action=get_leagues&APIkey=${APIkey}`, {
+  method: "GET",
+})
+  .then((response) => response.json())
+  .then((result) => {
+    result.forEach(({ league_name, league_id }) => {
+      const option = document.createElement("option");
+      option.text = league_name;
+      option.value = league_id;
+      leagueDropdown.appendChild(option);
+      console.log(option.value);
+    });
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+
+leagueDropdown.addEventListener("change", () => {
+  const selectedLeagueId = leagueDropdown.value;
+  fetch(
+    `https://apiv3.apifootball.com/?action=get_standings&league_id=${selectedLeagueId}&APIkey=${APIkey}`,
+    {
+      method: "GET",
+    }
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      const tableContent = document.getElementById("table-content");
+      tableContent.innerHTML = "";
+
+      result.forEach(
+        ({
+          overall_league_position,
+          team_name,
+          overall_league_W,
+          overall_league_D,
+          overall_league_L,
+          overall_league_GF,
+          overall_league_GA,
+          overall_league_PTS,
+        }) => {
+          const row = document.createElement("tr");
+          row.innerHTML = `
+      <td class="align-middle">${overall_league_position}</td>
+      <td class="align-middle">${team_name}</td>
+      <td class="align-middle">${overall_league_W}</td>
+      <td class="align-middle">${overall_league_D}</td>
+      <td class="align-middle">${overall_league_L}</td>
+      <td class="align-middle">${overall_league_GF}</td>
+      <td class="align-middle">${overall_league_GA}</td>
+      <td class="align-middle">${overall_league_PTS}</td>
+    `;
+          tableContent.appendChild(row);
+        }
+      );
+    })
+    .catch((error) => {
+      console.error("Error fetching leagues:", error);
+    });
+});
 
 countryDropdown.addEventListener("change", () => {
   const selectedCountryId = countryDropdown.value;
